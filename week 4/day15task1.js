48dd9f42688cd69261ae974addccf83a11e1d707
jemmaasryan <92854542+jemmaasryan@users.noreply.github.com>
2022-10-02 19:51:44 +0400

    Update day15task1.js

--- week 4/day15task1.js
+++ week 4/day15task1.js
@@ -8,21 +8,22 @@
  */
 
 let matrix = [[4,  6, 11, 8,  1 ],
-              [5,  0, 2,  27, 9 ],
-              [3,  2, 18, 4,  33],
-              [21, 8, 5,  8,  1 ] ];
+              [5,  3, 2,  27, 9 ],
+              [3,  0, 18, 4,  33],
+              [21, 5, 5,  8,  1 ] ];
 
 function findSmallest(matrix) {
-    let smallest;
+    let smallest = [];
     for(let row = 0; row < matrix.length; row++){ 
-        for(let col = 0; col < matrix.length; col++){
-            if(matrix[row][col] < matrix[row+1][col+1]) {
-                matrix[row][col] = smallest;
-                // return smallest;
-            }
-        }
+        smallest.push(Math.min(...matrix[row]))
     }
-    return smallest
+
+    let min = Math.min(...smallest)
+    let final = []
+    if(smallest.indexOf(min)+1 >= matrix.length){
+        console.log(matrix[0])
+    } 
+    final = matrix[smallest.indexOf(min) +1]
+    return final
 }
 console.log(findSmallest(matrix))
-
