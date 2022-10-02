990230fdaf828394466cb468fc8a6614db181940
jemmaasryan <92854542+jemmaasryan@users.noreply.github.com>
2022-10-02 17:04:27 +0400

    Update day19task2.js

--- week 4/day19task2.js
+++ week 4/day19task2.js
@@ -8,27 +8,19 @@
 *****     *
 
 */
-let n = 5;
-let string = "";
-for (let i = 1; i <= n; i++) {
-  for (let j = 0; j < i; j++) {
-    string += "*";
+function stars(n) {
+  let str = ""
+  let smb = "*"
+  for(let i = 1; i <= n; i++) {
+    for(let j = n; j >= -1; j--) {
+      if(i+j < n) {
+        str += smb
+      } else if(i+j > n) {
+        str += smb
+      } str += " " 
+    }
+    str += "\n"
   }
-  string += "\n";
+  return str
 }
-console.log(string);
-
-let n3 = 5;
-let string3 = "";
-for (let i = 1; i <= n3; i++) {
-  // printing spaces
-  for (let j = 0; j < n3-i; j++) {
-    string3 += " ";
-  }
-  // printing star
-  for (let k = 0; k < i; k++) {
-    string3 += "*";
-  }
-  string3 += "\n";
-}
-console.log(string3);
+console.log(stars(5))
