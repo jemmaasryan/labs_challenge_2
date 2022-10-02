24bb2dc0cabb73dd3390b33e2db572e990051680
jemmaasryan <92854542+jemmaasryan@users.noreply.github.com>
2022-10-02 17:58:05 +0400

    Update day16task2.js

--- week 4/day16task2.js
+++ week 4/day16task2.js
@@ -1,1 +1,18 @@
+
 // Day 16 Task 2
+
+
+function fizz(n, m) {
+    for(let i = n; i <= m; i++) {
+        if(i%3 == 0 && i%5 == 0) {
+            console.log(i, "FizzBuzz")
+        } else if(i%3 == 0) {
+            console.log(i, "Fizz")
+        } else if(i%5 == 0) {
+            console.log(i, "Buzz")
+        } else console.log(i)
+    }
+    return 
+}
+
+console.log(fizz(0, 15))
